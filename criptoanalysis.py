f61e45046714f53e3a2c6c6b7d22397e8a859802
Alessandro Bresciani <abresciani@abresciani-ltm2.internal.salesforce.com>
2017-12-16 22:50:45 +0000

    first commit

---
+++ criptoanalysis.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import quandl
+from datetime import datetime
+
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+py.init_notebook_mode(connected=True)
+
+def get_quandl_data(quandl_id):
+    '''Download and cache Quandl dataseries'''
+    cache_path = '{}.pkl'.format(quandl_id).replace('/','-')
+    try:
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(quandl_id))
+    except (OSError, IOError) as e:
+        print('Downloading {} from Quandl'.format(quandl_id))
+        df = quandl.get(quandl_id, returns="pandas")
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(quandl_id, cache_path))
+    return df
+
+# Pull Kraken BTC price exchange data
+btc_usd_price_kraken = get_quandl_data('BCHARTS/KRAKENUSD')
+btc_usd_price_kraken.head()
+
+# Chart the BTC pricing data
+btc_trace = go.Scatter(x=btc_usd_price_kraken.index, y=btc_usd_price_kraken['Weighted Price'])
+py.iplot([btc_trace])
+
+# Pull pricing data for 3 more BTC exchanges
+exchanges = ['COINBASE','BITSTAMP','ITBIT']
+
+exchange_data = {}
+
+exchange_data['KRAKEN'] = btc_usd_price_kraken
+
+for exchange in exchanges:
+    exchange_code = 'BCHARTS/{}USD'.format(exchange)
+    btc_exchange_df = get_quandl_data(exchange_code)
+    exchange_data[exchange] = btc_exchange_df
+    
+def merge_dfs_on_column(dataframes, labels, col):
+    '''Merge a single column of each dataframe into a new combined dataframe'''
+    series_dict = {}
+    for index in range(len(dataframes)):
+        series_dict[labels[index]] = dataframes[index][col]
+        
+    return pd.DataFrame(series_dict)
+
+# Merge the BTC price dataseries' into a single dataframe
+btc_usd_datasets = merge_dfs_on_column(list(exchange_data.values()), list(exchange_data.keys()), 'Weighted Price')
+
+btc_usd_datasets.tail()
