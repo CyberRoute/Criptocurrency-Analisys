992ece789f06fe60b0518c3777416ddfaddb4377
Alessandro Bresciani <Alessandro@iMac.local>
2017-12-17 10:21:54 +0000

    adding pricing by exchange, altcoins and correlations 2016/2017

--- criptoanalysis.py
+++ criptoanalysis.py
@@ -52,7 +52,142 @@ def merge_dfs_on_column(dataframes, labels, col):
         
     return pd.DataFrame(series_dict)
 
+
 # Merge the BTC price dataseries' into a single dataframe
 btc_usd_datasets = merge_dfs_on_column(list(exchange_data.values()), list(exchange_data.keys()), 'Weighted Price')
 
 btc_usd_datasets.tail()
+
+def df_scatter(df, title, seperate_y_axis=False, y_axis_label='', scale='linear', initial_hide=False):
+    '''Generate a scatter plot of the entire dataframe'''
+    label_arr = list(df)
+    series_arr = list(map(lambda col: df[col], label_arr))
+    
+    layout = go.Layout(
+        title=title,
+        legend=dict(orientation="h"),
+        xaxis=dict(type='date'),
+        yaxis=dict(
+            title=y_axis_label,
+            showticklabels= not seperate_y_axis,
+            type=scale
+        )
+    )
+    
+    y_axis_config = dict(
+        overlaying='y',
+        showticklabels=False,
+        type=scale )
+    
+    visibility = 'visible'
+    if initial_hide:
+        visibility = 'legendonly'
+        
+    # Form Trace For Each Series
+    trace_arr = []
+    for index, series in enumerate(series_arr):
+        trace = go.Scatter(
+            x=series.index, 
+            y=series, 
+            name=label_arr[index],
+            visible=visibility
+        )
+        
+        # Add seperate axis for the series
+        if seperate_y_axis:
+            trace['yaxis'] = 'y{}'.format(index + 1)
+            layout['yaxis{}'.format(index + 1)] = y_axis_config    
+        trace_arr.append(trace)
+
+    fig = go.Figure(data=trace_arr, layout=layout)
+    py.iplot(fig)
+
+# Remove "0" values
+btc_usd_datasets.replace(0, np.nan, inplace=True)
+
+# Plot all of the BTC exchange prices
+df_scatter(btc_usd_datasets, 'Bitcoin Price (USD) By Exchange')
+
+# Calculate the average BTC price as a new column
+btc_usd_datasets['avg_btc_price_usd'] = btc_usd_datasets.mean(axis=1)
+
+# Plot the average BTC price
+btc_trace = go.Scatter(x=btc_usd_datasets.index, y=btc_usd_datasets['avg_btc_price_usd'])
+py.iplot([btc_trace])
+
+# Define Poloniex API Helper Functions (will download and cache JSON data from a provided URL.)
+def get_json_data(json_url, cache_path):
+    '''Download and cache JSON data, return as a dataframe.'''
+    try:        
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(json_url))
+    except (OSError, IOError) as e:
+        print('Downloading {}'.format(json_url))
+        df = pd.read_json(json_url)
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(json_url, cache_path))
+    return df
+
+base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'
+start_date = datetime.strptime('2015-01-01', '%Y-%m-%d') # get data from the start of 2015
+end_date = datetime.now() # up until today
+pediod = 86400 # pull daily data (86,400 seconds per day)
+
+def get_crypto_data(poloniex_pair):
+    '''Retrieve cryptocurrency data from poloniex'''
+    json_url = base_polo_url.format(poloniex_pair, start_date.timestamp(), end_date.timestamp(), pediod)
+    data_df = get_json_data(json_url, poloniex_pair)
+    data_df = data_df.set_index('date')
+    return data_df
+
+# Download Trading Data From Poloniex
+altcoins = ['ETH','LTC','XRP','ETC','STR','DASH','SC','XMR','XEM']
+
+altcoin_data = {}
+for altcoin in altcoins:
+    coinpair = 'BTC_{}'.format(altcoin)
+    crypto_price_df = get_crypto_data(coinpair)
+    altcoin_data[altcoin] = crypto_price_df
+
+# Calculate USD Price as a new column in each altcoin dataframe
+for altcoin in altcoin_data.keys():
+    altcoin_data[altcoin]['price_usd'] =  altcoin_data[altcoin]['weightedAverage'] * btc_usd_datasets['avg_btc_price_usd']
+
+# Merge USD price of each altcoin into single dataframe 
+combined_df = merge_dfs_on_column(list(altcoin_data.values()), list(altcoin_data.keys()), 'price_usd')
+# Add BTC price to the dataframe
+combined_df['BTC'] = btc_usd_datasets['avg_btc_price_usd']
+
+# Chart all of the altocoin prices
+df_scatter(combined_df, 'Cryptocurrency Prices (USD)', seperate_y_axis=False, y_axis_label='Coin Value (USD)', scale='log')
+
+# Calculate the pearson correlation coefficients for cryptocurrencies in 2016
+combined_df_2016 = combined_df[combined_df.index.year == 2016]
+combined_df_2016.pct_change().corr(method='pearson')
+
+def correlation_heatmap(df, title, absolute_bounds=True):
+    '''Plot a correlation heatmap for the entire dataframe'''
+    heatmap = go.Heatmap(
+        z=df.corr(method='pearson').as_matrix(),
+        x=df.columns,
+        y=df.columns,
+        colorbar=dict(title='Pearson Coefficient'),
+    )
+    
+    layout = go.Layout(title=title)
+    
+    if absolute_bounds:
+        heatmap['zmax'] = 1.0
+        heatmap['zmin'] = -1.0
+        
+    fig = go.Figure(data=[heatmap], layout=layout)
+    py.iplot(fig)
+
+
+correlation_heatmap(combined_df_2016.pct_change(), "Cryptocurrency Correlations in 2016")
+
+combined_df_2017 = combined_df[combined_df.index.year == 2017]
+combined_df_2017.pct_change().corr(method='pearson')
+
+correlation_heatmap(combined_df_2017.pct_change(), "Cryptocurrency Correlations in 2017")
